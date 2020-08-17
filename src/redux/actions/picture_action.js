ea7e80bf0836f6d203a54a29d3d286ae3915a70a
Emeka Osuagwu <emekaosuagwuandela@gmail.com>
2020-08-17 22:32:48 +0100

    adding init functionality

--- src/redux/actions/picture_action.js
+++ src/redux/actions/picture_action.js
@@ -1,9 +1,9 @@
 import {nasa_api_instance, backend_api_instance} from "../../utility/axios"
 
-const { REACT_APP_API_KEY } = process.env;
+const { REACT_APP_NASA_API_KEY } = process.env;
 
-export const getRemotePicture = (request_body) => async dispatch => {
-    return nasa_api_instance.get('/pictures')
+export const getRemotePicture = (date) => async dispatch => {
+    return nasa_api_instance.get(`/planetary/apod?api_key=${REACT_APP_NASA_API_KEY}&date=${date}`)
 }
 
 export const getPictures = (request_body) => async dispatch => {
--- src/screen/Home.js
+++ src/screen/Home.js
@@ -1,31 +1,58 @@
 import React, { useEffect, useState } from 'react';
 
+import moment from 'moment'
 import { connect } from 'react-redux'
 import * as actions from '../redux/actions' 
 
-function App({getPictures}) {
+function App({getPictures, getRemotePicture}) {
 
+	const [picture, set_picture] =  useState(null)
+	const [selected_date, set_selected_date] =  useState(moment().format("YYYY-MM-DD"))
 
-	const [pictures, set_pictures] =  useState([])
-
+	//   const getNextDate = (date) => {
+	// 	let prevDate = new Date(date);
+	  
+	// 	prevDate = prevDate.setDate(prevDate.getDate() + 1);
+	  
+	// 	return `${prevDate.getFullYear()}-${prevDate.getMonth() + 1}-${prevDate.getDate()}`;
+	// };
+	  
 	useEffect(() => {
-
 		async function fetchData(){
-			await getPictures()
-			.then(response => {
-				console.log(response.data)
-			})
+			await fetchRemotePicture(selected_date)
 		}
-
 		fetchData()
+	}, [true])
 
-	})
 
+	const fetchRemotePicture = async (date) => {
+		await getRemotePicture(date)
+		.then(response => {
+			set_picture(response.data)
+		})
+	}
+
+	const getPrevDate = async () => {
+		const new_date = moment(selected_date).subtract(1, 'day').format("YYYY-MM-DD")
+		await fetchRemotePicture(new_date)
+		set_selected_date(new_date)
+	};
+	  
+
+	const getNextDate = async () => {
+		const new_date = moment(selected_date).add(1, 'day').format("YYYY-MM-DD")
+		await fetchRemotePicture(new_date)
+		set_selected_date(new_date)
+	};
+	  
 	return (
 		<div className="App">
 			<header className="App-header">
-				working
+				{JSON.stringify(picture)}
 			</header>
+
+			<h1 onClick={() => getPrevDate()}>back</h1>
+			<h1 onClick={() => getNextDate()}>forward</h1>
 		</div>
 	);
 }
