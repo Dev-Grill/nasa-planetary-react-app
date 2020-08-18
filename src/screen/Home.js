8bb3593d3e8d2601df70cbf502b89e82f613c800
Emeka Osuagwu <emekaosuagwuandela@gmail.com>
2020-08-18 20:43:19 +0100

    setup bootrap

--- src/screen/Home.js
+++ src/screen/Home.js
@@ -4,6 +4,8 @@ import moment from 'moment'
 import { connect } from 'react-redux'
 import * as actions from '../redux/actions' 
 
+import Carousel from 'react-bootstrap/Carousel'
+
 function App({getPictures, loadDatabase, getPrevDate, getNextDate, findOrCreate, likePicture, disPicture, pictureReducer}) {
 
 	const { picture, likes } = pictureReducer
@@ -24,32 +26,45 @@ function App({getPictures, loadDatabase, getPrevDate, getNextDate, findOrCreate,
 		disPicture(picture)
 	}
 
-	if(picture){
-		return (
-			<div className="App">
-				<header className="App-header">
-					{picture.date} 
-					{picture.favorite && <span> liked</span>}
-				</header>
+	return (
+		<Carousel>
+			<Carousel.Item>
+				<img
+				className="d-block w-100"
+				src="holder.js/800x400?text=First slide&bg=373940"
+				alt="First slide"
+				/>
+				<Carousel.Caption>
+				<h3>First slide label</h3>
+				<p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+				</Carousel.Caption>
+			</Carousel.Item>
+			<Carousel.Item>
+				<img
+				className="d-block w-100"
+				src="holder.js/800x400?text=Second slide&bg=282c34"
+				alt="Third slide"
+				/>
 
-				<h1 onClick={() => getPrevDate(picture.date)}>back</h1>
-				<h1 onClick={() => getNextDate(picture.date)}>forward</h1>
-	
-				<h1 onClick={() => handleLike(picture)}>Like Picture</h1>
-				<h1 onClick={() => handleUnlike(picture)}>Unlike Picture</h1>
-				
-				likes
-				<header className="App-header">
-					{JSON.stringify(likes)}
-				</header>
-			</div>
-		)
-	}
-	else {
-		return (
-			<h1>loanding</h1>
-		)
-	}
+				<Carousel.Caption>
+				<h3>Second slide label</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+				</Carousel.Caption>
+			</Carousel.Item>
+			<Carousel.Item>
+				<img
+				className="d-block w-100"
+				src="holder.js/800x400?text=Third slide&bg=20232a"
+				alt="Third slide"
+				/>
+
+				<Carousel.Caption>
+				<h3>Third slide label</h3>
+				<p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+				</Carousel.Caption>
+			</Carousel.Item>
+			</Carousel>
+	)
 
 }
 
